450095640b44fc9b2c1b98f4ee4118e3607f61f7
keonwooChoi <easter9211@gmail.com>
2021-06-11 13:15:21 +0000

    <feat>saint 모델

--- myeongsoo/code2/args.py
+++ myeongsoo/code2/args.py
@@ -2,64 +2,149 @@ import os
 import argparse
 
 
-def parse_args(mode='train'):
+def parse_args(mode="train"):
     parser = argparse.ArgumentParser()
 
-    
-    parser.add_argument('--seed', default=42, type=int, help='seed')
-    
-    parser.add_argument('--device', default='cuda', type=str, help='cpu or gpu')
+    parser.add_argument("--seed", default=42, type=int, help="seed")
 
-    parser.add_argument('--data_dir', default='/opt/ml/input/data/train_dataset', type=str, help='data directory')
-    parser.add_argument('--asset_dir', default='/opt/ml/asset/', type=str, help='data directory')
-    parser.add_argument('--feature_type', default='ver1', type=str, help='feature combination you choose')
+    parser.add_argument("--device", default="cuda", type=str, help="cpu or gpu")
 
-    parser.add_argument('--file_name', default='train_data.csv', type=str, help='train file name')
-    
-    parser.add_argument('--model_dir', default='/opt/ml/models/', type=str, help='model directory')
-    parser.add_argument('--model_name', default='model.pt', type=str, help='model file name')
-    parser.add_argument('--output_name', default='output.csv', type=str, help='model file name')
+    parser.add_argument(
+        "--data_dir",
+        default="/opt/ml/input/data/train_dataset",
+        type=str,
+        help="data directory",
+    )
+    parser.add_argument(
+        "--asset_dir", default="/opt/ml/asset/", type=str, help="data directory"
+    )
+    parser.add_argument(
+        "--feature_type",
+        default="ver1",
+        type=str,
+        help="feature combination you choose",
+    )
 
-    parser.add_argument('--output_dir', default='/opt/ml/output/sweep', type=str, help='output directory')
-    parser.add_argument('--test_file_name', default='test_data.csv', type=str, help='test file name')
-    
-    parser.add_argument('--max_seq_len', default=500, type=int, help='max sequence length')
-    parser.add_argument('--num_workers', default=4, type=int, help='number of workers')
+    parser.add_argument(
+        "--file_name", default="train_data.csv", type=str, help="train file name"
+    )
+
+    parser.add_argument(
+        "--model_dir", default="/opt/ml/models/", type=str, help="model directory"
+    )
+    parser.add_argument(
+        "--model_name", default="model.pt", type=str, help="model file name"
+    )
+    parser.add_argument(
+        "--output_name", default="output.csv", type=str, help="model file name"
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        default="/opt/ml/output/sweep",
+        type=str,
+        help="output directory",
+    )
+    parser.add_argument(
+        "--test_file_name", default="test_data.csv", type=str, help="test file name"
+    )
+
+    parser.add_argument(
+        "--max_seq_len", default=1000, type=int, help="max sequence length"
+    )
+    parser.add_argument("--num_workers", default=4, type=int, help="number of workers")
 
     # 모델
-    parser.add_argument('--hidden_dim', default=256, type=int, help='hidden dimension size')
-    parser.add_argument('--n_layers', default=4, type=int, help='number of layers')
-    parser.add_argument('--n_heads', default=4, type=int, help='number of heads')
-    parser.add_argument('--drop_out', default=0.4, type=float, help='drop out rate')
-    parser.add_argument('--interaction_type', default='problem_number', type=str, help='how to embed the interaction')
+    parser.add_argument(
+        "--hidden_dim", default=256, type=int, help="hidden dimension size"
+    )
+    parser.add_argument("--n_layers", default=4, type=int, help="number of layers")
+    parser.add_argument("--n_heads", default=4, type=int, help="number of heads")
+    parser.add_argument("--drop_out", default=0.4, type=float, help="drop out rate")
+    parser.add_argument(
+        "--interaction_type",
+        default="problem_number",
+        type=str,
+        help="how to embed the interaction",
+    )
     # 훈련
-    parser.add_argument('--n_epochs', default=50, type=int, help='number of epochs')
-    parser.add_argument('--batch_size', default=32, type=int, help='batch size')
-    parser.add_argument('--lr', default=0.0001, type=float, help='learning rate')
-    parser.add_argument('--clip_grad', default=100, type=int, help='clip grad')
-    parser.add_argument('--patience', default=3, type=int, help='for early stopping')
-    parser.add_argument('--sep_grade', default=True, type=bool, help='for seperate the sequence by grade')
-    parser.add_argument('--augment_rate', default=0.7, type=float, help='percent to augment')    
-    parser.add_argument('--augment', default=True, type=bool, help='for augment the seq length various')
-    parser.add_argument('--log_steps', default=50, type=int, help='print log per n steps')
-    parser.add_argument('--cv_strategy', default=True, type=bool, help='for apply cv strategy for train/valid split')
-    parser.add_argument('--cv_test', default=True, type=bool, help='for apply cv strategy for train/valid split')
-    parser.add_argument('--slide', default=True, type=bool, help='for apply cv strategy for train/valid split')
-    parser.add_argument('--window_size', default=15, type=int, help='for apply cv strategy for train/valid split')
+    parser.add_argument("--n_epochs", default=50, type=int, help="number of epochs")
+    parser.add_argument("--batch_size", default=1, type=int, help="batch size")
+    parser.add_argument("--lr", default=0.0001, type=float, help="learning rate")
+    parser.add_argument("--clip_grad", default=100, type=int, help="clip grad")
+    parser.add_argument("--patience", default=3, type=int, help="for early stopping")
+    parser.add_argument(
+        "--sep_grade",
+        default=True,
+        type=bool,
+        help="for seperate the sequence by grade",
+    )
+    parser.add_argument(
+        "--augment_rate", default=0.7, type=float, help="percent to augment"
+    )
+    parser.add_argument(
+        "--augment", default=True, type=bool, help="for augment the seq length various"
+    )
+    parser.add_argument(
+        "--log_steps", default=50, type=int, help="print log per n steps"
+    )
+    parser.add_argument(
+        "--cv_strategy",
+        default=True,
+        type=bool,
+        help="for apply cv strategy for train/valid split",
+    )
+    parser.add_argument(
+        "--cv_test",
+        default=True,
+        type=bool,
+        help="for apply cv strategy for train/valid split",
+    )
+    parser.add_argument(
+        "--slide",
+        default=True,
+        type=bool,
+        help="for apply cv strategy for train/valid split",
+    )
+    parser.add_argument(
+        "--window_size",
+        default=15,
+        type=int,
+        help="for apply cv strategy for train/valid split",
+    )
 
-    parser.add_argument('--hm_ts', default=4, type=int, help='for apply cv strategy for train/valid split')
-    parser.add_argument('--hs_tr', default=2, type=int, help='for apply cv strategy for train/valid split')
-    parser.add_argument('--test2train', default=True, type=bool, help='for apply cv strategy for train/valid split')
+    parser.add_argument(
+        "--hm_ts",
+        default=4,
+        type=int,
+        help="for apply cv strategy for train/valid split",
+    )
+    parser.add_argument(
+        "--hs_tr",
+        default=2,
+        type=int,
+        help="for apply cv strategy for train/valid split",
+    )
+    parser.add_argument(
+        "--test2train",
+        default=True,
+        type=bool,
+        help="for apply cv strategy for train/valid split",
+    )
 
     ### 중요 ###
-    parser.add_argument('--model', default='lstmattn', type=str, help='model type')
-    parser.add_argument('--optimizer', default='adamW', type=str, help='optimizer type')
-    parser.add_argument('--weight_decay', default=0.0001, type=float, help='optimizer weight decay')
+    parser.add_argument("--model", default="lstmattn", type=str, help="model type")
+    parser.add_argument("--optimizer", default="adamW", type=str, help="optimizer type")
+    parser.add_argument(
+        "--weight_decay", default=0.0001, type=float, help="optimizer weight decay"
+    )
 
-    parser.add_argument('--scheduler', default='plateau', type=str, help='scheduler type')
+    parser.add_argument(
+        "--scheduler", default="plateau", type=str, help="scheduler type"
+    )
 
-    parser.add_argument('--loss_type', default='bce', type=str, help='loss type')
-    parser.add_argument('--delta', default=2, type=float, help='scheduler type')
+    parser.add_argument("--loss_type", default="bce", type=str, help="loss type")
+    parser.add_argument("--delta", default=2, type=float, help="scheduler type")
     args = parser.parse_args()
 
     return args
